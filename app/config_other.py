e2e77c8f07419a31d5179e535bd7ee910672979d
yajen091488 <tsaiyajen0914@g.ncu.edu.tw>
2022-12-26 01:17:11 +0800

    刊登列表動物修改,時間格式調整

--- app/config_other.py
+++ app/config_other.py
@@ -1,11 +1,14 @@
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_uploads import IMAGES, UploadSet, configure_uploads
+from flask_moment import Moment
 login_manager = LoginManager()
 bcrypt = Bcrypt()
 photos = UploadSet("photos", IMAGES)
+moment = Moment()
 def init(app):
     login_manager.init_app(app)
     login_manager.login_view = 'index_views.login'
     bcrypt.init_app(app)
     configure_uploads(app, photos)
+    moment.init_app(app)
